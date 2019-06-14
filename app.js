7efe8e3be1cf8f3109651c03c4617ca8151c1d74
mengkko <aappcc1@icloud.com>
2019-06-14 15:24:37 +0900

    MVC Pattern 2

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-import express from "express"
-import morgan from "morgan"
-import helmet from "helmet"
-import bodyParser from "body-parser"
+import express      from "express"
+import morgan       from "morgan"
+import helmet       from "helmet"
+import bodyParser   from "body-parser"
 import cookieParser from "cookie-parser"
-import userRouter from './routers/userRouter'
-import videoRouter from './routers/videoRouter'
+import userRouter   from './routers/userRouter'
+import videoRouter  from './routers/videoRouter'
 import globalRouter from './routers/globalRouter'
+import routes       from './routes'
+
 const app = express()
 
 // ----------------------------- middleware 호출 시작
@@ -18,9 +20,9 @@ app.use(morgan('dev'))
 // ----------------------------- middleware 끝
 
 // ----------------------------- route 시작 
-app.use("/", globalRouter)
-app.use("/video", videoRouter)
-app.use("/user", userRouter)
+app.use(routes.home, globalRouter)
+app.use(routes.users, userRouter)
+app.use(routes.videos, videoRouter)
 // ----------------------------- route 종료
 
 export default app
