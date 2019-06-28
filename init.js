50f4641e9da4ab69e32d5321a0fec2047f09920d
Mengkko <aappcc1@icloud.com>
2019-06-28 19:01:04 +0900

    Comment Model Create

--- init.js
+++ init.js
@@ -3,8 +3,9 @@ import app from './app.js'
 import dotenv from 'dotenv'
 dotenv.config()
 import "./models/Video"
+import "./models/Comment"
 
-const PORT = process.env.PORT || 4000
+const PORT = process.env.PORT
 
 const handleListening = () => console.log(`Listening on: http://localhost:${PORT}`)
 
