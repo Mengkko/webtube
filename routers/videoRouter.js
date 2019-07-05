ff957be9492cd2877f9fed501e6f9f25fb981eff
mengkko <aappcc1@icloud.com>
2019-07-05 16:25:00 +0900

    Install Eslint

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,30 +1,29 @@
-import express      from 'express'
-import routes       from '../routes'
-import { 
-    videos, 
-    getUpload,
-    postUpload, 
-    videoDetail, 
-    getEditVideo,
-    postEditVideo, 
-    deleteVideo 
-}                   from '../controllers/videoController';
-import { uploadVideo } from '../middlewares';
+import express from "express";
+import routes from "../routes";
+import {
+  getUpload,
+  postUpload,
+  videoDetail,
+  getEditVideo,
+  postEditVideo,
+  deleteVideo
+} from "../controllers/videoController";
+import { uploadVideo } from "../middlewares";
 
-const videoRouter = express.Router()
+const videoRouter = express.Router();
 
 // Upload Video
-videoRouter.get(routes.upload,          getUpload);
-videoRouter.post(routes.upload,         uploadVideo,    postUpload);
+videoRouter.get(routes.upload, getUpload);
+videoRouter.post(routes.upload, uploadVideo, postUpload);
 
 // Video Detail
-videoRouter.get(routes.videoDetail(),   videoDetail);
+videoRouter.get(routes.videoDetail(), videoDetail);
 
 // Edit Video
-videoRouter.get(routes.editVideo(),     getEditVideo);
-videoRouter.post(routes.editVideo(),    postEditVideo)
+videoRouter.get(routes.editVideo(), getEditVideo);
+videoRouter.post(routes.editVideo(), postEditVideo);
 
 // Delete Video
-videoRouter.get(routes.deleteVideo(),     deleteVideo);
+videoRouter.get(routes.deleteVideo(), deleteVideo);
 
-export default videoRouter
+export default videoRouter;
