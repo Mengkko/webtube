a737fb6042625e206d7c5a3eaae44e72126b9f5f
Mengkko <aappcc1@icloud.com>
2019-07-24 23:39:08 +0900

    #8.3 Enter Fullscreen Exit Fullscreen

--- assets/js/videoPlayer.js
+++ assets/js/videoPlayer.js
@@ -1,7 +1,8 @@
 const videoContainer = document.getElementById("jsVideoPlayer");
 const videoPlyer = document.querySelector("#jsVideoPlayer video");
 const playButton = document.getElementById("jsPlayButton");
-const jsVolumeButton = document.getElementById("jsVolumeButton");
+const VolumeButton = document.getElementById("jsVolumeButton");
+const fullscrnButton = document.getElementById("jsFullScreen");
 
 function handlePlayClick() {
   if (videoPlyer.paused) {
@@ -16,15 +17,29 @@ function handlePlayClick() {
 function handleVolumeClick() {
   if (videoPlyer.muted) {
     videoPlyer.muted = false;
-    jsVolumeButton.innerHTML = '<i class="fas fa-volume-up"></i>';
+    VolumeButton.innerHTML = '<i class="fas fa-volume-up"></i>';
   } else {
     videoPlyer.muted = true;
-    jsVolumeButton.innerHTML = '<i class="fas fa-volume-mute"></i>';
+    VolumeButton.innerHTML = '<i class="fas fa-volume-mute"></i>';
   }
+}
+
+function exitFullScreen() {
+  fullscrnButton.innerHTML = '<i class="fas fa-expand"></i>';
+  fullscrnButton.addEventListener("click", goFullScreen);
+  document.exitFullscreen();
+}
+
+function goFullScreen() {
+  videoContainer.webkitRequestFullscreen();
+  fullscrnButton.innerHTML = '<i class="fas fa-compress"></i>';
+  fullscrnButton.removeEventListener("click", goFullScreen);
+  fullscrnButton.addEventListener("click", exitFullScreen);
 }
 function init() {
   playButton.addEventListener("click", handlePlayClick);
-  jsVolumeButton.addEventListener("click", handleVolumeClick);
+  VolumeButton.addEventListener("click", handleVolumeClick);
+  fullscrnButton.addEventListener("click", goFullScreen);
 }
 
 if (videoContainer) {
